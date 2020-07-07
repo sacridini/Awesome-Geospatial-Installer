1f20442caf6149bcf4be78eb94102762d2c26538
sacridini <eduardolacerdageo@gmail.com>
2020-07-06 23:12:33 -0300

    updated main.sh

--- main.sh
+++ main.sh
@@ -1,30 +1,71 @@
 #!/bin/bash
 
 # ---------------------------------- [ start ] ------------------------------------
-printf "\n\n ### Running PLANGEA Installer ### \n\n"
-printf "- Installing Dialog \n\n"
-sudo apt-get install -y dialog
+printf "\n ### Running PLANGEA Installer ### \n"
+sudo apt-get install -y dialog; echo "Installed dialog without errors." >> plangea_installer.log
 
 welcomeMsg() {
-  dialog --title "PLANGEA" --infobox "PLANGEA Installer v0.1 \n\n\n Use the keyboard to select items" 9 45; sleep 1; clear 
+  dialog --title "PLANGEA" --msgbox "PLANGEA Installer v0.1 \n\n\n Pronto para instalar o PLANGEA? " 9 45;
+  clear 
+}
+
+showMenu() {
+ escolha=$(dialog --menu "What to install?:" 10 60 3 \
+        1 "Utils (htop, tmux, gdal, R, Python, etc)"\
+        2 "R Packages" \
+        3 "Gurobi" --stdout);
+ clear
 }
 
 installUtils() {
-  sudo apt-get install -y build-essential
-  sudo apt-get install -y git
-  sudo apt-get install -y tmux
-  sudo apt-get install -y htop
-  sudo apt-get install -y vim
-  sudo apt-get install -y libudunits2-dev
-  sudo apt-get install -y libssl-dev
-  sudo apt-get install -y libcairo2-dev
-  sudo apt-get install -y libopenblas-dev 
-  sudo apt-get install -y liblapack-dev
-  sudo apt-get install -y python-dev
-  sudo apt-get install -y r-base-dev
-  sudo apt-get install -y gdal-bin
-  sudo apt-get install -y libgdal-dev
+  sudo apt-get install -y build-essential; echo "Installed build essential without errors." >> plangea_installer.log
+  sudo apt-get install -y git; echo "Installed git without errors." >> plangea_installer.log
+  sudo apt-get install -y tmux; echo "Installed tmux without errors." >> plangea_installer.log
+  sudo apt-get install -y htop; echo "Installed htop without errors." >> plangea_installer.log
+  sudo apt-get install -y vim; echo "Installed vim without errors." >> plangea_installer.log
+  sudo apt-get install -y libudunits2-dev; echo "Installed libudunits2 without errors." >> plangea_installer.log
+  sudo apt-get install -y libssl-dev; echo "Installed libssl without errors." >> plangea_installer.log
+  sudo apt-get install -y libcairo2-dev; echo "Installed libcairo without errors." >> plangea_installer.log
+  sudo apt-get install -y libopenblas-dev;  echo "Installed libopenblas without errors." >> plangea_installer.log
+  sudo apt-get install -y liblapack-dev; echo "Installed liblapack without errors." >> plangea_installer.log
+  sudo apt-get install -y python-dev; echo "Installed python without errors." >> plangea_installer.log
+  sudo apt-get install -y r-base-dev; echo "Installed R without errors." >> plangea_installer.log
+  sudo apt-get install -y gdal-bin; echo "Installed gdal (bin) without errors." >> plangea_installer.log
+  sudo apt-get install -y libgdal-dev; echo "Installed gdal (dev) without errors." >> plangea_installer.log
 }
 
-welcomeMsg
-installUtils
+installPlangea() {
+  instp=$(dialog --title "PLANGEA" --menu "What branch to install?" 0 0 3 \
+        1 "master"\
+        2 "dev" --stdout); clear
+  
+  plangea_path=$(dialog --title "PLANGEA" --inputbox "Especifique o path para fazer o clone do pacote: " 0 0 "./" --stdout); clear
+  
+  if [instp == 1]
+  then
+    cd $plangea_path
+    git clone https://github.com/IIS-Rio/plangea-pkg
+  else
+     cd $plangea_path
+     git clone https://github.com/IIS-Rio/plangea-pkg
+     cd plangea-pkg
+     git checkout dev
+     cd ..
+  fi
+  echo "Create a plangea clone at $(plangea_path)" >> plangea_installer.log
+}
+
+finish() {
+  dialog --title "PLANGEA" --infobox "Instalação concluída com sucesso!" 5 40;
+  sleep 2;
+  clear;
+  dialog --title "PLANGEA" --infobox "Team Tarrasque Riders 2020" 5 30;
+  sleep 3;
+  clear
+}
+
+# welcomeMsg
+# installUtils
+# showMenu
+installPlangea
+finish
